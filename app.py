014362912035c73b9f7932eb6bb1a56f7f6de163
Charles Rhodes <crhodes76@users.noreply.github.com>
2025-02-11 10:51:59 -0500

    added more print statements and opening the url in a web browser

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import requests
 import json
 from pprint import pprint
-
+import webbrowser
 url = 'https://api.github.com/users/crhodes76/repos'
+
 print('Making a request to:', url)
 response = requests.get(url)
-print('Getting response:', response)
+
+print('Getting response status code:', response.status_code)
 data = response.json()
-# Extract and print the name attribute from each repository
-#for repo in data:
-#    print(repo['name'])
+
+print('The URL', response.url)
+the_url = response.url
+print('Opening the URL in a web browser')
+webbrowser.open(the_url)
+
+raw_data = response.text
+print('Raw data from the response:')
+print(raw_data)
+
+print('Dictionary data from json.loads:')
+dictionary_data = json.loads(raw_data)
+print(dictionary_data)
 
 # Generate a new list of repository names
 print('generating a list of repository names')
